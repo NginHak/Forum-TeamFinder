9ff92ec429d8515dd75a70721b6ae4fa9ecbb80c
unknown <nginhak@gmail.com>
2019-05-01 22:34:49 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ForumAndTeamFinder</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+        <!-- Styles -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">FTF</a>
+                </div>
+                <ul class="nav navbar-nav">
+
+                <li class="active"><a href="#">Home</a></li>
+
+                </ul>
+            </div>
+        </nav>
+
+    </body>
+</html>
